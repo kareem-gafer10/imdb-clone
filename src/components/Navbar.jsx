d5212ba8bf895da3ee98e5ed51bf0f2bae7538ca
kareem-gafer10 <kareemgafer28.com>
2024-01-21 08:32:58 +0200

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+
+import { navbarList } from "@/data/data";
+import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
 
-export default Navbar
+  const searchParams = useSearchParams();
+  const generate = searchParams.get("generate");
+  return (
+    <div className="flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4 gap-x-10">
+      {navbarList.map(({title,param }) => (
+        <Link href={`/?generate=${param}`} key={title}
+        className={` hover:text-amber-600 font-semibold
+        ${generate &&generate === param &&"underline underline-offset-8 decoration-4  decoration-amber-500 rounded-lg"}`}>
+          {title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Navbar;
--- src/data/data.js
+++ src/data/data.js
@@ -3,5 +3,14 @@ import { BsFillInfoCircleFill } from "react-icons/bs";
 
 export const headerList = [
   { title: "HOME", icon: <AiFillHome className=" text-2xl" />, link: "/" },
-  { title: "ABOUT", icon: <BsFillInfoCircleFill className=" text-2xl" />, link: "/about" },
+  {
+    title: "ABOUT",
+    icon: <BsFillInfoCircleFill className=" text-2xl" />,
+    link: "/about",
+  },
+];
+
+export const navbarList = [
+  { title: "Trending", param: "Trending" },
+  { title: "Top Rated", param: "TopRated" },
 ];
