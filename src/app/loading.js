8c2ec4a891eb32814d5cb61d3ca9d96c28aa78bc
kareem-gafer10 <kareemgafer28.com>
2024-01-21 11:11:37 +0200

    add loading

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex justify-center">
+        <img className="h-96" src="spinner.svg" alt="loading..." />
+      </div>
+  )
+}
+
+export default loading
+
