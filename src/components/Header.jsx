44bd479905a4401a493e8a82107d346d0520254e
kareem-gafer10 <kareemgafer28.com>
2024-01-21 07:00:12 +0200

    create header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { headerList } from "@/data/data"
+import Link from "next/link"
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center py-5 px-3">
+
+      <div className="flex gap-x-5 sm:gap-x-10 ">
+        {headerList.map(({title, icon, link})=>(
+          <Link key={link} href={link} className="hover:text-amber-600">
+            <p className="sm:hidden">{icon}</p>
+            <p className="hidden sm:block">{title}</p>
+          </Link>
+        ))}
+      </div>
+
+      <h2 className=" text-2xl ">
+            <Link href="/" className="flex items-center gap-x-2">
+            <p className="bg-amber-500 font-bold py-1 px-2 rounded-lg">IMDB</p>
+            <p className=" text-xl hidden sm:block">Clone</p>
+            </Link>
+      </h2>
+
+
+    </div>
+  )
+}
+
+export default Header
+
+
+
---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+
+export const headerList = [
+  { title: "HOME", icon: <AiFillHome className=" text-2xl" />, link: "/" },
+  { title: "ABOUT", icon: <BsFillInfoCircleFill className=" text-2xl" />, link: "/about" },
+];
